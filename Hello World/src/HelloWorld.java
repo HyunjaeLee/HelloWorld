601e594d032502b3f352392885df25c7fde5508a
Hyunjae,Lee <guswoqkrtk@gmail.com>
2014-07-12 23:25:48 +0900

    Second Commit

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -1,3 +1,9 @@
+/*
+ * Hyunjae Lee
+ * Eclipse Plug-In EGit Test
+ * Hello World
+ * Hello Java8
+ * */
 public class HelloWorld {
 public static void main(String[] args){
 	System.out.println("HelloWorld");
